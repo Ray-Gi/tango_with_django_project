187775e630ef7c06b75a68e0b98ce9d37e084407
Rayan Mohammed <2865753m@student.gla.ac.uk>
2024-01-18 15:55:43 +0000

    index.html template added, view altered

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 # exercise 3 
 def about(request):
