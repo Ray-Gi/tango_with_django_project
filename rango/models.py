475d3a4564a6b12ac4382cbc6d45432c82d01076
Rayan Mohammed <2865753m@student.gla.ac.uk>
2024-01-20 18:53:10 +0000

    category pages and url mappings using slugs added

--- rango/models.py
+++ rango/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
 
-    def dave(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
+    # query Category and retrieve top 5 categories
     category_list = Category.objects.order_by('-likes')[:5]
 
     context_dict = {}
@@ -11,7 +12,28 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
     context_dict = {'boldmessage':'This tutorial has been put together by Ray Mohammed'}
     return render(request, 'rango/about.html', context = context_dict)
 
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    # try to find a category name from the slug given
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        # get all pages belonging to this category
+        # filter returns : list of page objects 
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
