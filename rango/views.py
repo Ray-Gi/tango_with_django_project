c69ae33923230f23166aa5767bc21fbcbf1fc2a0
Rayan Mohammed <2865753m@student.gla.ac.uk>
2024-01-20 17:50:23 +0000

    Upto 6.2, Data-driven webpage feature first added

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
 
     return render(request, 'rango/index.html', context=context_dict)
 
-# exercise 3 
 def about(request):
     context_dict = {'boldmessage':'This tutorial has been put together by Ray Mohammed'}
     return render(request, 'rango/about.html', context = context_dict)
