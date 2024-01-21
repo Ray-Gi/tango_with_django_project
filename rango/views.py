7352e217ca638b31d4a8e66ab343aec66315cb76
Rayan Mohammed <2865753m@student.gla.ac.uk>
2024-01-21 01:20:21 +0000

    added Category form

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+from django.shortcuts import redirect
 
 def index(request):
     # query Category and retrieve top 5 categories
@@ -40,3 +42,21 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form':form})
+
+
